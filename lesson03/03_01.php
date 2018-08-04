e5bfb6ac86446be22b0289dcbc8a5bdb8bba2b24
Artyom Stepanov <banana2fun@gmail.com>
2018-08-04 11:30:11 +0700

    compleate lesson03

---
+++ lesson03/03_01.php
@@ -0,0 +1,14 @@
+<?php
+
+$input = fopen ("php://stdin", "r");
+$output = fopen("php://stdout", "w");
+
+fwrite($output, "Введите n: ");
+fscanf($input, "%d", $n);
+
+$p = 1;
+for ($i = 1; $i <= $n; $i++) {
+    $p *= 1 + 1 / ($i * $i);
+}
+
+fprintf($output, "Произведение %d первых сомножителей равно %f", $n, $p);
